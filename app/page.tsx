3a66ba239c389fc5ef5c90db350ad4f70c1324eb
gourab8389 <deyg6988@gmail.com>
2024-06-21 21:36:41 +0530

    add todo

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
 import AddTodo from "@/components/shared/AddTodo";
-import Image from "next/image";
+import Todo from "@/components/shared/Todo";
+import { prisma } from "@/utils/prisma";
 
-export default function Home() {
+
+async function getData() {
+  const data = await prisma.todo.findMany({
+    select: {
+      title: true,
+      id: true,
+      isCompleted: true,
+    },
+    orderBy: {
+      createdAt: "desc"
+    },
+  });
+  return data;
+}
+
+export default async function Home() {
+  const data = await getData();
   return (
     <div className="w-screen py-20 flex justify-center flex-col items-center">
       <span className="text-3xl font-extrabold uppercase">
@@ -13,6 +30,14 @@ export default function Home() {
       </h1>
       <div className="flex justify-center flex-col items-center w-[1000px]">
       <AddTodo/>
+      <div className="flex flex-col gap-5 items-center justify-center mb-10 w-full ">
+        {data.map((todo, id)=>(
+          <div className="w-full 
+          " key={id}>
+            <Todo/>
+          </div>
+        ))}
+      </div>
       </div>
     </div>
   );
